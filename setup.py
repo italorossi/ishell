f92d945e26e9a30c03c9f3adf6c9761d79b565fc
Italo Rossi <italorossib@gmail.com>
2014-04-19 23:12:16 -0300

    Updating project version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ishell',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Ítalo Rossi',
     author_email='italorossib@gmail.com',
     description='Build Interactive Shells with Python',

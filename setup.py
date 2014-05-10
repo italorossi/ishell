a68c949d40feb1787ec54e3364051e40528be163
Italo Rossi <italorossib@gmail.com>
2014-05-10 00:32:10 -0300

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ishell',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Ítalo Rossi',
     author_email='italorossib@gmail.com',
     description='Build Interactive Shells with Python',

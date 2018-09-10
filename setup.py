6316e0bd0395856d11c6f2be7c4762c2a0fd65fb
Italo Rossi <italorossib@gmail.com>
2018-09-10 11:04:50 -0300

    For now, python 2.7, 3.6 and 3.7 are supported.
    Signed-off-by: Italo Rossi <italorossib@gmail.com>

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Topic :: Utilities',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
